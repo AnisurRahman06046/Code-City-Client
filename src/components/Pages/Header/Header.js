95e19b9f453ad313c4502d782711416a564bbb1c
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-10-26 04:30:26 +0600

    logo added

--- src/components/Pages/Header/Header.js
+++ src/components/Pages/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoPic from "../../../assets/logo.png";
 
 const Header = () => {
   return (
     <div className="navbar bg-primary  text-primary-content">
       <div className="flex-1 ">
+        <img style={{ height: "30px" }} src={logoPic} alt="" />
         <Link className="btn btn-ghost normal-case text-xl">CodeCity</Link>
       </div>
       <div className="flex-none ">
