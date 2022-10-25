cf5749a406adc1e6f66ccf732cb8bd8ed7e938e7
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-10-26 00:37:53 +0600

    project setup , home page,header,layout

---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello Code Hungrier</h1>
+          <p className="py-6">
+            This platform is for you to harvest your knowledge . Here you can
+            find programming languaged tutorial.Explore the courses and be an
+            efficient coder.Code your dreem , believe on yourself and stay with
+            us.
+          </p>
+          <button className="btn btn-primary">Explore Courses</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
