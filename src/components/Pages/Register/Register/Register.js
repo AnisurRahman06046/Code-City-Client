08ae1bf88bf8bee4904597d3b1eb15773b246829
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-10-27 14:01:18 +0600

    login register added

--- src/components/Pages/Register/Register/Register.js
+++ src/components/Pages/Register/Register/Register.js
@@ -1,9 +1,82 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const url = form.url.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    form.reset();
+    console.log(email, password, name, url);
+  };
   return (
-    <div>
-      <h1>this is register page</h1>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col ">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register!</h1>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo Url</span>
+              </label>
+              <input
+                type="text"
+                name="url"
+                placeholder="Photo Url"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                required
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                required
+                className="input input-bordered"
+              />
+              <label className="label">
+                <Link to="/login" className="label-text-alt link link-hover">
+                  Already have an account? Log In
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
